0f58170c91488e769fb907eae722ee827da9ac9c
spykelion <ndilionel6@gmail.com>
2021-07-21 18:42:35 +0100

    added base styles for landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import "./App.css";
+import styles from "./app.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {Button, Container }from 'react-bootstrap';
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container fluid>
+        <Header/>
+        
+        <Switch>
+          <Route path="/login">
+            <div>Login component</div>
+          </Route>
+          <Route path="/signup">
+          <div>Signup component</div>
+          </Route>
+          <Route path="/">
+          <div>Welcome</div>
+          <Button className={styles.error}>My button</Button>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Nav,
+  NavDropdown,
+  Navbar,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import styles from "../styles/header.module.css";
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <Row>
+      <Navbar bg="light" expand="lg">
+        <Col>
+          {" "}
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              width=""
+              height=""
+              className="d-inline-block align-top"
+              alt="Stocka logo"
+            />
+          </Navbar.Brand>
+        </Col>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Col>
+            <Nav className="me-auto">
+              <Nav.Link href="/about">Features</Nav.Link>
+              <NavDropdown title="Pricing" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#"> Inventory Management </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/login">
+                  Analytics
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">
+                  Reports
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Col>
+          <Col sm lg="2">
+            <Link to="/login" className="btn btn-outline-primary">
+              LOGIN
+            </Link>
+          </Col>
+          <Col sm lg="2">
+            <Link to="/signup" className="btn btn-primary">SIGNUP</Link>
+          </Col>
+        </Navbar.Collapse>
+      </Navbar>
+    </Row>
+  );
+}
+
+export default Header;
